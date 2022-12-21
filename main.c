a05c718820a3196cfda470387dc3043f3ad66a62
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2022-12-21 22:52:44 +0200

    forget everything and start a new one from now

--- main.c
+++ main.c
@@ -1,44 +1,56 @@
 #include<stdio.h>
 #include<string.h>
+//print number of each column
 void header(int col)
+{
+    for (int i=1; i<=col; i++)
     {
-        for (int i=1;i<=col;i++)
-        {
-            printf("  %d ",i);
-        }
-         printf("\n");
+        printf("  %d ",i);
     }
+    printf("\n");
+}
+//print a fixed boarder between each row
 void bar (int col)
 {
-    for (int i=0;i<col;i++)
+    for (int i=0; i<col; i++)
     {
         printf("|---");
     }
     printf("|\n");
 }
+//intializing the array
+void intial_arr (int n,int m,char a[][m])
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            a[i][j]=' ';
+        }
+    }
+}
+//print the whole grid of the game
+ void grid (int n,int m,char a[][m])
+    {
+        header(m);
+        bar(m);
+        int i, j;
+        for (i = 1; i <=n; i++)
+        {
+            for (j = 1; j <= m; j++)
+            {
+                printf("| %c ", a[i][j]);
+            }
+            printf("|\n");
+            bar(m);
+        }
+    }
 int main()
 {
-    int rows , col;
+    int rows, col;
     scanf("%d %d",&rows,&col);
     char scores[rows][col];
-    for (int i = 1; i <= rows; i++)
-    {
-        for (int j = 1; j <= col; j++)
-        {
-            scores[i][j]=' ';
-        }
-    }
-    header(col);
-    bar(col);
-    int i, j;
-    for (i = 1; i <=rows; i++)
-    {
-        for (j = 1; j <= col; j++)
-        {
-            printf("| %c ", scores[i][j]);
-        }
-        printf("|\n");
-        bar(col);
-    }
-   return 0;
+    intial_arr(rows,col,scores);
+    grid(rows,col,scores);
+    return 0;
 }
