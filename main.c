827f71655c811894367cea0f009c229a2fcef8e5
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2022-12-22 13:20:06 +0200

    the whole code with play function

--- main.c
+++ main.c
@@ -1,5 +1,52 @@
 #include<stdio.h>
 #include<string.h>
+struct {
+char player1;
+char player2;
+}color;
+int choose_col(int m,char a[][m])
+{
+   int num_of_col;
+   do {
+   printf("please choose the number of column you want\n");
+   scanf ("%d",&num_of_col);
+   }
+   while (a[1][num_of_col]!=' ' || num_of_col<1 || num_of_col>m);
+   return num_of_col;
+}
+int check_gameover (int m,char a[][m])
+{
+    for (int ii =1; ii<=m; ii++)
+    {
+        if (a[1][ii]==' '){
+        return 1 ;
+        }
+    }
+return 0;
+}
+void fill_grid(int turn,int num_of_col,int n,int m,char a[][m])
+{
+    for (int jj=n; jj>0;jj--)
+    {
+        if (a[jj][num_of_col]==' ')
+        {
+            if (turn%2==0){
+            a[jj][num_of_col]='X';}
+            else{
+            a[jj][num_of_col]='O';}
+        break;
+        }
+    }
+}
+void _incolor (char x)
+{
+    if (x=='y')
+        printf("\033[1;33m");
+    else if (x=='r')
+        printf("\033[1;31m");
+    else
+        printf("\033[0m");
+}
 //print number of each column
 void header(int col)
 {
@@ -31,7 +78,7 @@ void intial_arr (int n,int m,char a[][m])
 }
 //print the whole grid of the game
  void grid (int n,int m,char a[][m])
-    {
+{
         header(m);
         bar(m);
         int i, j;
@@ -45,6 +92,25 @@ void intial_arr (int n,int m,char a[][m])
             bar(m);
         }
     }
+
+void play(int n,int m,char a[][m])
+{
+    // using a counter to know who's player should play and to be helpful when using undo or redo
+    /*printf("please,choose your color \nyellow -> y\nred   -> r\n");
+    scanf ("\n%c",&color.player1);
+    if (color.player1=='y')
+        color.player2='r' ;
+    else
+        color.player2='y' ;*/
+   //check if the game is over or not as it should return 0 if it's over
+    int counter =0;
+    while (check_gameover (m,a))
+    {
+        fill_grid(counter,choose_col(m,a),n,m,a);
+        grid(n,m,a);
+        counter ++;
+    }
+}
 int main()
 {
     int rows, col;
@@ -52,5 +118,7 @@ int main()
     char scores[rows][col];
     intial_arr(rows,col,scores);
     grid(rows,col,scores);
+    play(rows,col,scores);
+
     return 0;
 }
